668687dea17d86040b7472618115dd9b877bf0ad
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-07-19 14:09:58 +0300

    Changed some login stuff

--- connect.php
+++ connect.php
@@ -4,7 +4,3 @@ $username = "root";
 $password = "";
 $dbname = "MIDI-Synthesiser";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
